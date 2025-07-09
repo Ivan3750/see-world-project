44324f13d729781c4db3a52e602e327d5e7aca13
danik00111 <kremenskiydaniil@gmail.com>
2025-07-09 19:28:07 +0300

    things

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import {  Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import './App.css';
-import React, {  lazy } from 'react';
+import { lazy } from 'react';
 import Favorites from './pages/Favorites';
 import CreateAdvert from './components/CreateAdvert';
 
@@ -8,7 +8,7 @@ import CreateAdvert from './components/CreateAdvert';
 const Home = lazy(() => import('./pages/Home')); 
 const Layout = lazy(() => import('./components/Layout')); 
 const Ogolosh = lazy(() => import('./pages/Ogolosh'));
-const Regestation = lazy(() => import('./pages/Regestation'));
+const Registration = lazy(() => import('./pages/Registration'));
 const LogIn = lazy(() => import('./pages/LogIn'));
 const Reset = lazy(() => import('./pages/Reset'));
 
@@ -21,7 +21,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/advert" element={<Ogolosh />} />
-          <Route path="/regestation" element={<Regestation />} />
+          <Route path="/Registration" element={<Registration />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/reset" element={<Reset />} />
           <Route path="/favorites" element={<Favorites/>} />
